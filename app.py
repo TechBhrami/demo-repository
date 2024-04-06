c514c1a616341e4c2bc61aab8d1723bf4b9e8e10
ROSHAN058 <DNB868@GMAIL.COM>
2024-04-06 17:52:49 +0000

    added certs

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello():
    return 'Hello, World!'
 
 if __name__ == "__main__":
-       app.run(ssl_context=('cert.pem', 'key.pem'))
+       app.run(ssl_context=('cert.pem', 'key.pem'),port=443)
